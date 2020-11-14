b93828df5f9b9907745063ece8cf6764d2b1a315
Mr. Robot <t0thkr1s@icloud.com>
2020-11-14 13:15:14 +0100

    add native library

---
+++ app/src/main/cpp/native_library.cpp
@@ -0,0 +1,50 @@
+#include <jni.h>
+#include <string>
+#include <iostream>
+#include <utility>
+
+using namespace std;
+
+string jstring2string(JNIEnv *env, jstring jStr) {
+    if (!jStr)
+        return "";
+
+    const jclass stringClass = env->GetObjectClass(jStr);
+    const jmethodID getBytes = env->GetMethodID(stringClass, "getBytes", "(Ljava/lang/String;)[B");
+    const auto stringJbytes = (jbyteArray) env->CallObjectMethod(jStr, getBytes,
+                                                                 env->NewStringUTF("UTF-8"));
+
+    auto length = (size_t) env->GetArrayLength(stringJbytes);
+    jbyte *pBytes = env->GetByteArrayElements(stringJbytes, nullptr);
+
+    std::string ret = std::string((char *) pBytes, length);
+    env->ReleaseByteArrayElements(stringJbytes, pBytes, JNI_ABORT);
+
+    env->DeleteLocalRef(stringJbytes);
+    env->DeleteLocalRef(stringClass);
+    return ret;
+}
+
+string hardcoreEncryption(JNIEnv *env, jstring text) {
+    // here is the plain text password, just in case...
+    string p = "supersecret";
+    char k = 'K';
+    string pass = jstring2string(env, text);
+    string output = pass;
+
+    for (int i = 0; i < pass.size(); i++)
+        output[i] = pass[i] ^ k;
+
+    return output;
+}
+
+jboolean checkPass(JNIEnv *env, jstring pass) {
+    return hardcoreEncryption(env, pass) == "8>;.98.(9.?";
+}
+
+extern "C"
+JNIEXPORT jboolean JNICALL
+Java_com_infosecadventures_allsafe_challanges_NativeLibrary_checkPassword(JNIEnv *env,
+                                                                          jstring password) {
+    return checkPass(env, password);
+}
