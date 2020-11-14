184a84661fc408a5be05a3767e8bf6275b71ea9e
Mr. Robot <t0thkr1s@icloud.com>
2020-11-14 18:19:43 +0100

    add secret broadcast receiver

---
+++ app/src/main/java/infosecadventures/allsafe/SecretReceiver.java
@@ -0,0 +1,28 @@
+package infosecadventures.allsafe;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Base64;
+import android.util.Log;
+import android.widget.Toast;
+
+import java.util.Arrays;
+
+public class SecretReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        try {
+            String secret = intent.getStringExtra("secret");
+            byte[] data = Base64.decode(secret, Base64.DEFAULT);
+            if (Arrays.equals(data, Base64.decode("YzEwZjAzNzM0YWZjYzhiOGZhMzk3YjJiODk1NGY2NzQ=", Base64.DEFAULT))) {
+                Toast.makeText(context, "Congratulations!", Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(context, "Sorry, that's not the flag!", Toast.LENGTH_LONG).show();
+            }
+        } catch (IllegalArgumentException e) {
+            Log.d("ALLSAFE", "Exception. Looks characters, we can't decode!");
+        }
+    }
+}
