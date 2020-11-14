f172677dd8213f983dc95d8f22f5540f8623aca1
Mr. Robot <t0thkr1s@icloud.com>
2020-11-14 18:17:59 +0100

    add contants

---
+++ app/src/main/java/infosecadventures/allsafe/Contants.kt
@@ -0,0 +1,9 @@
+package infosecadventures.allsafe
+
+object Constants {
+    const val GITHUB_URI = "https://www.github.com/t0thkr1s"
+    const val TWITTER_URI = "https://www.twitter.com/t0thkr1s"
+
+    const val BTC_ADDRESS = "bc1qd44kvj6zatjgn27n45uxd3nprzt6rm9x9g2yc8"
+    const val ETH_ADDRESS = "0x1835a58E866a668C48Ee63d32432C7Fe28aF54b4"
+}
