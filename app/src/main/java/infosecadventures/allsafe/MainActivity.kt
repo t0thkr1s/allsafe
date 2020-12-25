57c87c8cbc8ec003590bab7c58823d77e16d8399
Mr. Robot <t0thkr1s@icloud.com>
2020-12-25 17:54:08 +0100

    add fragment navigation

--- app/src/main/java/infosecadventures/allsafe/MainActivity.kt
+++ app/src/main/java/infosecadventures/allsafe/MainActivity.kt
@@ -27,16 +27,19 @@ class MainActivity : AppCompatActivity() {
         val drawer = findViewById<DrawerLayout>(R.id.drawer_layout)
         val navigationView = findViewById<NavigationView>(R.id.nav_view)
         mAppBarConfiguration = AppBarConfiguration.Builder(
+                R.id.nav_main,
                 R.id.nav_search,
                 R.id.nav_insecure_logging,
-                R.id.nav_deep_link_exploitation,
                 R.id.nav_hardcoded_credentials,
+                R.id.nav_sql_injection,
+                R.id.nav_deep_link_exploitation,
                 R.id.nav_weak_crypto,
                 R.id.nav_certificate_pinning,
                 R.id.nav_pin_bypass,
                 R.id.nav_root_detection,
                 R.id.nav_secure_flag_bypass,
                 R.id.nav_vulnerable_web_view,
+                R.id.nav_smali_patch,
                 R.id.nav_native_library,
                 R.id.nav_about)
                 .setOpenableLayout(drawer)
