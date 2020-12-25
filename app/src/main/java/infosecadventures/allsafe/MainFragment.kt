c2e5efbd632c45b009fd35c0e52cf819e34960f0
Mr. Robot <t0thkr1s@icloud.com>
2020-12-25 17:56:32 +0100

    add main fragment

---
+++ app/src/main/java/infosecadventures/allsafe/MainFragment.kt
@@ -0,0 +1,14 @@
+package infosecadventures.allsafe
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class MainFragment : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+}
