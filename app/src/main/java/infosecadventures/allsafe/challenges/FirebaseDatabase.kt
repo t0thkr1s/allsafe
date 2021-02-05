6caf2f54941bd16681de6640506e159e3510582c
Mr. Robot <t0thkr1s@icloud.com>
2021-02-05 20:35:00 +0100

    add firebase database task

---
+++ app/src/main/java/infosecadventures/allsafe/challenges/FirebaseDatabase.kt
@@ -0,0 +1,41 @@
+package infosecadventures.allsafe.challenges
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.fragment.app.Fragment
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
+import infosecadventures.allsafe.R
+import infosecadventures.allsafe.utils.SnackUtil
+
+
+class FirebaseDatabase : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+
+        val view: View = inflater.inflate(R.layout.fragment_firebase_database, container, false)
+
+        val execute: Button = view.findViewById(R.id.execute)
+        execute.setOnClickListener {
+            val database = Firebase.database
+            val reference = database.reference.child("secret")
+            reference.addValueEventListener(object : ValueEventListener {
+                override fun onDataChange(dataSnapshot: DataSnapshot) {
+                    SnackUtil.simpleMessage(requireActivity(), dataSnapshot.value as String)
+                }
+
+                override fun onCancelled(databaseError: DatabaseError) {
+                    SnackUtil.simpleMessage(requireActivity(), "Sorry, database error!")
+                }
+            })
+
+        }
+
+        return view
+    }
+}
