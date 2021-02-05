af228d9528f512bbdcfcbd3d5043e39e8f6e8507
Mr. Robot <t0thkr1s@icloud.com>
2021-02-05 16:01:26 +0100

    fix sql injection task

--- app/src/main/java/infosecadventures/allsafe/challenges/SQLInjection.kt
+++ app/src/main/java/infosecadventures/allsafe/challenges/SQLInjection.kt
@@ -11,6 +11,7 @@ import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.google.android.material.textfield.TextInputEditText
 import infosecadventures.allsafe.R
+import java.math.BigInteger
 import java.security.MessageDigest
 
 
@@ -27,29 +28,33 @@ class SQLInjection : Fragment() {
         val login: Button = view.findViewById(R.id.login)
         login.setOnClickListener {
 
-            val cursor: Cursor = db.rawQuery("select * from users where username = '" + username + "' and password = '" + password + "'", null)
-
+            val cursor: Cursor = db.rawQuery("select * from users where username = '" + username.text.toString() + "' and password = '" + md5(password.text.toString()) + "'", null)
+            val data = StringBuilder()
             if (cursor.count > 0) {
                 cursor.moveToFirst()
                 do {
-                    val id = cursor.getString(0)
                     val user = cursor.getString(1)
-                    val pw = cursor.getString(2)
-                    Toast.makeText(context, "ID: " + id + " User: " + user, Toast.LENGTH_LONG).show()
+                    val pass = cursor.getString(2)
+                    data.append("User: $user \nPass: $pass\n")
                 } while (cursor.moveToNext())
             }
             cursor.close()
-
+            Toast.makeText(context, data, Toast.LENGTH_LONG).show()
         }
         return view
     }
 
-    private fun md5(str: String): ByteArray = MessageDigest.getInstance("MD5").digest(str.toByteArray(Charsets.UTF_8))
+    private fun md5(input: String): String {
+        val md = MessageDigest.getInstance("MD5")
+        return BigInteger(1, md.digest(input.toByteArray())).toString(16).padStart(32, '0')
+    }
+
+
     private fun populateDatabase(): SQLiteDatabase {
         val db = requireActivity().openOrCreateDatabase("allsafe", android.content.Context.MODE_PRIVATE, null)
         db.execSQL("drop table if exists users")
         db.execSQL("create table users ( id integer primary key autoincrement, username text, password text )")
-        db.execSQL("insert into users ( id, username, password ) values (0, 'admin', 'b90708413dbbd54a670ab3f3f9026f9b')")
+        db.execSQL("insert into users ( id, username, password ) values (0, 'admin', '21232f297a57a5a743894a0e4a801fc3')")
         db.execSQL("insert into users ( id, username, password ) values (1, 'elliot.alderson', '3484cef7f6ff172c2cd278d3b51f3e66')")
         db.execSQL("insert into users ( id, username, password ) values (2, 'angela.moss', '0af58729667eace3883a992ef2b8ce29')")
         db.execSQL("insert into users ( id, username, password ) values (3, 'gideon.goddard', '65dc3431f8c5e3f0e249c5b1c6e3534d')")
