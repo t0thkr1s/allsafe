286f720e42c94cafa5971372b8156f3933c01119
Mr. Robot <t0thkr1s@icloud.com>
2021-02-07 20:47:13 +0100

    add arbitrary code execution task

---
+++ app/src/main/java/infosecadventures/allsafe/challenges/ArbitraryCodeExecution.java
@@ -0,0 +1,18 @@
+package infosecadventures.allsafe.challenges;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import infosecadventures.allsafe.R;
+
+public class ArbitraryCodeExecution extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_arbitrary_code_execution, container, false);
+    }
+}
