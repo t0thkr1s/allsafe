9df0d31cf1a47a31e4d13e408b8b5f156f07e3a4
Mr. Robot <t0thkr1s@icloud.com>
2021-02-07 20:43:15 +0100

    rename table name in db

--- app/src/main/java/infosecadventures/allsafe/challenges/SQLInjection.kt
+++ app/src/main/java/infosecadventures/allsafe/challenges/SQLInjection.kt
@@ -16,8 +16,7 @@ import java.security.MessageDigest
 
 
 class SQLInjection : Fragment() {
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
 
         val view: View = inflater.inflate(R.layout.fragment_sql_injection, container, false)
         val db = populateDatabase()
@@ -28,7 +27,7 @@ class SQLInjection : Fragment() {
         val login: Button = view.findViewById(R.id.login)
         login.setOnClickListener {
 
-            val cursor: Cursor = db.rawQuery("select * from users where username = '" + username.text.toString() + "' and password = '" + md5(password.text.toString()) + "'", null)
+            val cursor: Cursor = db.rawQuery("select * from user where username = '" + username.text.toString() + "' and password = '" + md5(password.text.toString()) + "'", null)
             val data = StringBuilder()
             if (cursor.count > 0) {
                 cursor.moveToFirst()
@@ -199,14 +198,14 @@ class SQLInjection : Fragment() {
 
     private fun populateDatabase(): SQLiteDatabase {
         val db = requireActivity().openOrCreateDatabase("allsafe", android.content.Context.MODE_PRIVATE, null)
-        db.execSQL("drop table if exists users")
-        db.execSQL("create table users ( id integer primary key autoincrement, username text, password text )")
-        db.execSQL("insert into users ( id, username, password ) values (0, 'admin', '21232f297a57a5a743894a0e4a801fc3')")
-        db.execSQL("insert into users ( id, username, password ) values (1, 'elliot.alderson', '3484cef7f6ff172c2cd278d3b51f3e66')")
-        db.execSQL("insert into users ( id, username, password ) values (2, 'angela.moss', '0af58729667eace3883a992ef2b8ce29')")
-        db.execSQL("insert into users ( id, username, password ) values (3, 'gideon.goddard', '65dc3431f8c5e3f0e249c5b1c6e3534d')")
-        db.execSQL("insert into users ( id, username, password ) values (4, 'tyrell.wellick', '6d2e1c6dd505a108cc7e19a46aa30a8a')")
-        db.execSQL("insert into users ( id, username, password ) values (5, 'darlene.alderson', 'd510b80eb22f8eb684f1a19681eb7bcf')")
+        db.execSQL("drop table if exists user")
+        db.execSQL("create table user ( id integer primary key autoincrement, username text, password text )")
+        db.execSQL("insert into user ( username, password ) values ('admin', '21232f297a57a5a743894a0e4a801fc3')")
+        db.execSQL("insert into user ( username, password ) values ('elliot.alderson', '3484cef7f6ff172c2cd278d3b51f3e66')")
+        db.execSQL("insert into user ( username, password ) values ('angela.moss', '0af58729667eace3883a992ef2b8ce29')")
+        db.execSQL("insert into user ( username, password ) values ('gideon.goddard', '65dc3431f8c5e3f0e249c5b1c6e3534d')")
+        db.execSQL("insert into user ( username, password ) values ('tyrell.wellick', '6d2e1c6dd505a108cc7e19a46aa30a8a')")
+        db.execSQL("insert into user ( username, password ) values ('darlene.alderson', 'd510b80eb22f8eb684f1a19681eb7bcf')")
         return db
     }
 }
