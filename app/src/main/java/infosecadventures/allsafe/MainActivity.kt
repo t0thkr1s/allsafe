ccb06f90656bc72dcd949533ac7eaff5ee8c4ae6
Mr. Robot <t0thkr1s@icloud.com>
2021-02-07 20:48:41 +0100

    add new tasks to fragment navigation

--- app/src/main/java/infosecadventures/allsafe/MainActivity.kt
+++ app/src/main/java/infosecadventures/allsafe/MainActivity.kt
@@ -28,12 +28,13 @@ class MainActivity : AppCompatActivity() {
         val navigationView = findViewById<NavigationView>(R.id.nav_view)
         mAppBarConfiguration = AppBarConfiguration.Builder(
                 R.id.nav_main,
-                R.id.nav_search,
                 R.id.nav_insecure_logging,
                 R.id.nav_hardcoded_credentials,
                 R.id.nav_firebase_database,
+                R.id.nav_insecure_shared_preferences,
                 R.id.nav_sql_injection,
                 R.id.nav_deep_link_exploitation,
+                R.id.nav_insecure_broadcast_receiver,
                 R.id.nav_weak_crypto,
                 R.id.nav_certificate_pinning,
                 R.id.nav_pin_bypass,
@@ -41,6 +42,7 @@ class MainActivity : AppCompatActivity() {
                 R.id.nav_secure_flag_bypass,
                 R.id.nav_vulnerable_web_view,
                 R.id.nav_permission_redelegation,
+                R.id.nav_arbitrary_code_execution,
                 R.id.nav_smali_patch,
                 R.id.nav_native_library,
                 R.id.nav_about)
