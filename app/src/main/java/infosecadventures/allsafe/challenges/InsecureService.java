f2f4e182201e9cea8359b871dd7c63de6dbaba9f
Mr. Robot <t0thkr1s@icloud.com>
2021-02-21 11:59:44 +0100

    add check for permissions

--- app/src/main/java/infosecadventures/allsafe/challenges/InsecureService.java
+++ app/src/main/java/infosecadventures/allsafe/challenges/InsecureService.java
@@ -1,11 +1,14 @@
 package infosecadventures.allsafe.challenges;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 
 import infosecadventures.allsafe.R;
@@ -16,7 +19,15 @@ public class InsecureService extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_insecure_service, container, false);
-        view.findViewById(R.id.start).setOnClickListener(v -> requireActivity().startService(new Intent(requireActivity(), RecorderService.class)));
+        view.findViewById(R.id.start).setOnClickListener(v -> {
+            if (ActivityCompat.checkSelfPermission(requireActivity(), Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED &&
+                    ActivityCompat.checkSelfPermission(requireActivity(), Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED &&
+                    ActivityCompat.checkSelfPermission(requireActivity(), Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(requireActivity(), new String[]{Manifest.permission.RECORD_AUDIO, Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE}, 0);
+            } else {
+                requireActivity().startService(new Intent(requireActivity(), RecorderService.class));
+            }
+        });
         return view;
     }
 }
