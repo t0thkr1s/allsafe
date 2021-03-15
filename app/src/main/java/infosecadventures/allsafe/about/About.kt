f6727ef3662b375d45b10edbe869c56826b7e02f
Mr. Robot <t0thkr1s@icloud.com>
2021-03-15 11:01:53 +0100

    kotlin extension removal modifications

--- app/src/main/java/infosecadventures/allsafe/about/About.kt
+++ app/src/main/java/infosecadventures/allsafe/about/About.kt
@@ -6,27 +6,29 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import infosecadventures.allsafe.BuildConfig
 import infosecadventures.allsafe.Constants
 import infosecadventures.allsafe.R
 import infosecadventures.allsafe.utils.ClipUtil
 import infosecadventures.allsafe.utils.SnackUtil
-import kotlinx.android.synthetic.main.fragment_about.view.*
 
 class About : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.fragment_about, container, false)
-        view.version.text = "Version: ${BuildConfig.VERSION_NAME}"
-        view.blog.setOnClickListener { startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(Constants.MEDIUM))) }
-        view.github.setOnClickListener { startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(Constants.GITHUB))) }
-        view.twitter.setOnClickListener { startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(Constants.TWITTER))) }
-        view.bitcoin.setOnClickListener {
+        val version = "Version: ${BuildConfig.VERSION_NAME}"
+        view.findViewById<TextView>(R.id.version).text = version
+        view.findViewById<Button>(R.id.blog).setOnClickListener { startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(Constants.MEDIUM))) }
+        view.findViewById<Button>(R.id.github).setOnClickListener { startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(Constants.GITHUB))) }
+        view.findViewById<Button>(R.id.twitter).setOnClickListener { startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(Constants.TWITTER))) }
+        view.findViewById<Button>(R.id.bitcoin).setOnClickListener {
             ClipUtil.copyToClipboard(requireActivity(), Constants.BTC_ADDRESS)
             SnackUtil.simpleMessage(requireActivity(), "Bitcoin address copied to clipboard!")
         }
-        view.ethereum.setOnClickListener {
+        view.findViewById<Button>(R.id.ethereum).setOnClickListener {
             ClipUtil.copyToClipboard(requireActivity(), Constants.ETH_ADDRESS)
             SnackUtil.simpleMessage(requireActivity(), "Ethereum address copied to clipboard!")
         }
