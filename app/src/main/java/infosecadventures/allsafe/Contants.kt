d1370df5cc413c679c53789ae6461ab578bc9908
Mr. Robot <t0thkr1s@icloud.com>
2025-03-20 16:15:36 +0100

    gradle upgrade

--- app/src/main/java/infosecadventures/allsafe/Contants.kt
+++ app/src/main/java/infosecadventures/allsafe/Contants.kt
@@ -4,7 +4,7 @@ object Constants {
 
     const val MEDIUM = "https://medium.com/infosec-adventures"
     const val GITHUB = "https://www.github.com/t0thkr1s"
-    const val TWITTER = "https://www.twitter.com/t0thkr1s"
+    const val X = "https://www.x.com/t0thkr1s"
 
     const val BTC_ADDRESS = "bc1qd44kvj6zatjgn27n45uxd3nprzt6rm9x9g2yc8"
     const val ETH_ADDRESS = "0x1835a58E866a668C48Ee63d32432C7Fe28aF54b4"
--- app/src/main/java/infosecadventures/allsafe/about/About.kt
+++ app/src/main/java/infosecadventures/allsafe/about/About.kt
@@ -9,21 +9,24 @@ import android.view.ViewGroup
 import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.fragment.app.Fragment
-import infosecadventures.allsafe.BuildConfig
 import infosecadventures.allsafe.Constants
 import infosecadventures.allsafe.R
 import infosecadventures.allsafe.utils.ClipUtil
 import infosecadventures.allsafe.utils.SnackUtil
+import androidx.core.net.toUri
 
 class About : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.fragment_about, container, false)
-        val version = "Version: ${BuildConfig.VERSION_NAME}"
+        val version = "Version: ${requireContext().applicationContext.packageManager.getPackageInfo(requireContext().applicationContext.packageName, 0).versionName}"
         view.findViewById<TextView>(R.id.version).text = version
-        view.findViewById<LinearLayout>(R.id.blog).setOnClickListener { startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(Constants.MEDIUM))) }
-        view.findViewById<LinearLayout>(R.id.github).setOnClickListener { startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(Constants.GITHUB))) }
-        view.findViewById<LinearLayout>(R.id.twitter).setOnClickListener { startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(Constants.TWITTER))) }
+        view.findViewById<LinearLayout>(R.id.blog).setOnClickListener { startActivity(Intent(Intent.ACTION_VIEW,
+            Constants.MEDIUM.toUri())) }
+        view.findViewById<LinearLayout>(R.id.github).setOnClickListener { startActivity(Intent(Intent.ACTION_VIEW,
+            Constants.GITHUB.toUri())) }
+        view.findViewById<LinearLayout>(R.id.x).setOnClickListener { startActivity(Intent(Intent.ACTION_VIEW,
+            Constants.X.toUri())) }
         view.findViewById<LinearLayout>(R.id.bitcoin).setOnClickListener {
             ClipUtil.copyToClipboard(requireActivity(), Constants.BTC_ADDRESS)
             SnackUtil.simpleMessage(requireActivity(), "Bitcoin address copied to clipboard!")
